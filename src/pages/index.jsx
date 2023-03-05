b6b19e51434da264aa1ef6ad8ec9eddca5d7a545
KIrill Igumenshchev <kirill.igum@gmail.com>
2023-03-04 23:34:19 -0700

    update lens

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -27,6 +27,9 @@ function Page() {
       <Head />
       <div className="flex flex-col">
         <h1>lens</h1>
+        <div>username: juju</div>
+        <div>sortest path: me-->bill-->Lydia-->juju</div>
+        <div>score: 82.5</div>
         <h1>ongoing loans</h1>
         <table>
           <tr>
